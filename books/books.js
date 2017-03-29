22ebbb9dd03350ddd20ebe20dc07bc196e5941bd
julia_lebed <yulyalebed86@gmail.com>
2017-03-29 12:40:14 +0300

    books

---
+++ books/books.js
@@ -0,0 +1,52 @@
+const express = require('express'),
+		app = express(),
+		bodyParser = require('body-parser');
+
+app.use(express.static('./public/'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+let id = 2;
+
+const books = [
+	{
+		id: 0,
+		title: "book1",
+		author: "Samanta",
+		description: "description for this book",
+		published: 2000,
+	},
+
+	{
+		id: 1,
+		title: "history-book",
+		author: "David",
+		description: "Historical overview of events",
+		published: 1780,
+	}
+];
+
+app.get('/books', (req, res) => {
+	res.send(books);
+});
+
+app.post('/book', (req, res) =>{
+	const book = req.body;
+	book.id = id++;
+	books.push(book);
+
+	res.send(book);
+});
+
+app.get('/book/id', (req,res) => {
+	let bookId = parseFloat(req.params.id, 10);
+	console.log(bookId);
+	let index = books.findIndex(book => book.id === bookId);
+	console.log(index);
+	res.send(books[index]);
+});
+
+app.listen(5000, () => {
+	console.log("Listen app - 5000");
+});
---
+++ books/public/custom.js
@@ -0,0 +1,130 @@
+;(function(){
+	let booksContainer = document.querySelector(".books-container"),
+		addBookContainer = document.querySelector(".add-book-container"),
+		singleBook = document.querySelector(".single-book"),
+		mainPage = document.querySelector("#main-page"),
+		addBook = document.querySelector("#addBook"),
+		form = document.querySelector(".add-book-container form");
+
+
+	fetch("/books").
+		then(res => res.json()).
+		then(res => {
+			console.log(res);
+			createBookHtml(res);
+		}).
+		catch(e => console.error(e));
+
+
+	function createBookHtml(book) {
+
+		let fragment = document.createDocumentFragment();
+
+		book.forEach(el => {
+
+			let itemWrapper = document.createElement("div"),
+				bookTitle = document.createElement("h2"),
+				bookTitleLink = document.createElement("a"),
+				bookDescription = document.createElement("p"),
+				bookAuthor = document.createElement("p"),
+				bookPublished = document.createElement("p");
+
+			itemWrapper.className = "book-item";
+			bookTitle.className = "title";
+			bookDescription.className = "description";
+			bookAuthor.className = "author";
+			bookPublished.className = "published";
+			bookTitleLink.setAttribute("href", `/book/${el.id}`);
+			bookTitleLink.setAttribute("data-id", el.id);
+
+			bookTitleLink.textContent = `Title: ${el.title}`;
+			bookDescription.textContent = `Description: ${el.description}`;
+			bookAuthor.textContent = `Author: ${el.author}`;
+			bookPublished.textContent = `Published: ${el.published}`;
+
+			bookTitle.appendChild(bookTitleLink);
+			itemWrapper.appendChild(bookTitle);
+			itemWrapper.appendChild(bookDescription);
+			itemWrapper.appendChild(bookAuthor);
+			itemWrapper.appendChild(bookPublished);
+
+			fragment.appendChild(itemWrapper);
+		});
+		
+		booksContainer.appendChild(fragment);
+	}
+
+	addBook.addEventListener("click", showForm);
+	mainPage.addEventListener("click", showBooks);
+	form.addEventListener("submit", addNewBook);
+	booksContainer.addEventListener("click", singleBookPage);
+
+	function showForm(ev) {
+		ev.preventDefault();
+		addBookContainer.hidden = false;
+		booksContainer.hidden = true;
+		singleBook.hidden = true;
+	}
+
+	function showBooks(ev) {
+		ev.preventDefault();
+		addBookContainer.hidden = true;
+		booksContainer.hidden = false;
+		singleBook.hidden = true;
+	}
+
+	function addNewBook(ev) {
+		ev.preventDefault();
+
+		let title = form.querySelector("#title").value;
+
+		let	description = form.querySelector("#description").value;
+
+		let	author = form.querySelector("#author").value;
+
+		let	published = form.querySelector("#published").value;
+
+		fetch("/book", {
+			method: "POST",
+			body: JSON.stringify({ title, description, author, published }),
+			headers: {
+				"content-type": "application/json"
+			}
+		}).
+		then(res => res.json()).
+		then(res => {
+			console.log(res);
+		}).
+		catch(e => console.log(e));
+
+		form.querySelector("#title").value = "";
+		form.querySelector("#description").value = "";
+		form.querySelector("#author").value = "";
+		form.querySelector("#published").value = "";
+	}
+
+	function singleBookPage(ev) {
+		ev.preventDefault();
+
+		if(ev.target.tagName !== 'A') return;
+
+		addBookContainer.hidden = true;
+		booksContainer.hidden = true;
+		singleBook.hidden = false;
+
+		let singleTitle = document.querySelector('.single-title'),
+			singleDescr = document.querySelector('.single-description'),
+			singleAuthor = document.querySelector('.single-author'),
+			singlePublished = document.querySelector('.single-published');
+
+		let bookId = ev.target.getAttribute('data-id');
+		console.log(bookId);
+
+		fetch(`/book/${bookId}`).
+		then(res => res.json()).
+		then(res => {
+			console.log(res);
+		}).
+		catch(e => console.error(e));
+	}
+})();
